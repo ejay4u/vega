0e19d699f95b786f1d8a8e1937100ee9861edcb6
Jude Abormegah <abordem@gmail.com>
2021-02-20 15:36:23 +0100

    create new vehicle

--- Mapper/MappingProfile.cs
+++ Mapper/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using vega.Controllers.Resources;
 using vega.Models;
@@ -8,9 +9,22 @@ namespace vega.Mapper
     {
         public MappingProfile()
         {
+            //Domain to API Resources
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+            .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
+            .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.Id)));
+
+
+            //API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+            .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+            .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+            .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+            .ForMember(v => v.Features, opt => opt.Ignore());
+            //.ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new Feature { Id = id })));
         }
     }
 }
---
+++ Models/FeatureVehicle.cs
@@ -0,0 +1,8 @@
+namespace vega.Models
+{
+    public class FeatureVehicle
+    {
+        public int FeaturesId { get; set; }
+        public int VehiclesId { get; set; }
+    }
+}
